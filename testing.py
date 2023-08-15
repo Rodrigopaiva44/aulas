5d14a2ea674dd7d9d7bfaf57814228ff69c781ab
Digo <rodrigomatheuspaiva7@gmail.com>
2023-08-15 11:04:28 -0300

    review defs

--- testing.py
+++ testing.py
@@ -1,7 +1,54 @@
-def meow(n: int):
-    for _ in range(n):
-        print("meow")
+# Define a function
+def __hello():
+    print("hello")
 
 
-number = input("N: ")
-meow(number)
+name = input("Whats your name? ")
+__hello()
+print(name)
+
+
+# Creating our own parameters
+def _hello(n):
+    print("hello", n)
+
+
+name = input("Whats your name? ")
+_hello(name)
+
+
+# optional parameter
+def hello(n="World"):
+    print("hello", n)
+
+
+name = input("Whats your name? ")
+hello(name)
+
+# Why use main?
+
+
+def main():
+    name = input("Whats your name? ")
+    hello(name)
+
+
+def hello(n="World"):
+    print("hello", n)
+
+
+main()
+
+
+# returning values
+
+def main():
+    x = int(input("Whats x: "))
+    print("x squared is", square(x))
+
+
+def square(num):
+    return num * num
+
+
+main()
